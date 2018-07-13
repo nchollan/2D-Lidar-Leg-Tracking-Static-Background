ead6dea7c8452997b7ac2e00bc103540ca710397
nchollan <nchollan@gmail.com>
2018-07-13 13:41:19 -0400

    Update mark.py

--- mark.py
+++ mark.py
@@ -4,17 +4,29 @@ import numpy as np
 from sensor_msgs.msg import LaserScan
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
-numMsgs=0
-f1=[]
-movementThreshold=0.5
-legsThreshold=0.6
-perLegThreshold=0.2#0.1
-pubMarker = rospy.Publisher('visualization_marker',Marker, queue_size=1000)
-pubMarker2 = rospy.Publisher('visualization_marker2',Marker, queue_size=1000)
 
+numMsgs=0				#Keeps track of how many scan messages have been received
+f1=[]					#The first scan frame on startup
+movementThreshold=0.5	#How far should a point be from where it was in frame 1 before its considered a moving object
+legsThreshold=0.6		#How far away from the average of all moving points should a persons legs be
+pubMarker = rospy.Publisher('visualization_marker',Marker, queue_size=1000)#Publisher for the markers
+
+#Input:  Point(), Point()
+#		pt1 = The first Point()
+#		pt2 = The second Point()
+#Output: Distance from pt1 to pt2
 def distanceTo(pt1,pt2):
-
 	return math.hypot(pt1.x-pt2.x,pt1.y-pt2.y)
+
+
+#Input: int, int, int, int, dbl, dbl
+#		markerId = integer to represent a unique Marker()
+#		r = integer to represent red color
+#		g = integer to represent green color
+#		b = integer to represent blue color
+#		x = double to represent x position
+#		y = integer to represent y position
+#Output: Marker()
 def makeMarker(markerId,r,g,b,x,y):
 	marker = Marker()
 	marker.header.frame_id = "laser"
@@ -32,6 +44,14 @@ def makeMarker(markerId,r,g,b,x,y):
 	marker.pose.position.x=x
 	marker.pose.position.y=y
 	return marker
+
+#Input: int, int, int, int, dbl, dbl
+#		markerId = integer to represent a unique Marker()
+#		r = integer to represent red color
+#		g = integer to represent green color
+#		b = integer to represent blue color
+#		pts = list of Points() to represent x and y positions
+#Output: Marker()
 def makeSphereList(markerId,r,g,b,pts):
 	marker = Marker()
 	marker.type = marker.SPHERE_LIST
@@ -48,62 +68,52 @@ def makeSphereList(markerId,r,g,b,pts):
 	marker.lifetime = rospy.Duration(0.5)
 	marker.points=pts
 	return marker
+
+#Input: LaserScan
+#		data = LaserScan Data
+#Output: None
 def callback(data):
-	global numMsgs, f1, movementThreshold, legsThreshold, perLegThreshold, pubMarker, pubMarker2
-	numMsgs+=1
+	global numMsgs, f1, movementThreshold, legsThreshold, pubMarker, pubMarker2
+	numMsgs+=1	 								#Keeps track of how many scan messages have been received
 	if(numMsgs==1):
-		f1=data.ranges
+		f1=data.ranges 							#Grab the first frame on startup to use 
 	else:
-		f2=data.ranges
+		f2=data.ranges 							#Grab frames to compare to f1 to search for movement
 		i=0
-		positionsXY=[]
-		xTot=0
-		yTot=0
-		k=-1.65806281567
-		for i in range(0,1520):
+		positionsXY=[] 							#Points of movement in scan
+		xTot=0 									#Total of all x movement positions
+		yTot=0 									#Total of all y movement positions
+		k=-1.65806281567 						#Lowest radian angle measurement of lidar 
+		for i in range(0,1520): 				#1521 is len(data.ranges)
 			if(abs(f2[i]-f1[i])>movementThreshold):
-				movePoint=Point()
-				movePoint.x=f2[i]*(math.cos(k))
-				movePoint.y=f2[i]*(math.sin(k))
-				xTot+=movePoint.x
+				movePoint=Point() 				#Represent distance and angle as a point
+				movePoint.x=f2[i]*(math.cos(k)) #Calculate x position
+				movePoint.y=f2[i]*(math.sin(k))	#Calculate y position
+				xTot+=movePoint.x 				
 				yTot+=movePoint.y
 				positionsXY.append(movePoint)
-			k+=0.00218022723
+			k+=0.00218022723 					#Lidar range = -1.65806281567 to 1.65806281567
+							 					#k = 2*1.65806281567/len(data.ranges)
 		if(len(positionsXY)>0):
+			#Calculate the average x,y position of all movement
 			avgPos= Point()
 			avgPos.x=xTot/len(positionsXY)
 			avgPos.y=yTot/len(positionsXY)
 			pubMarker.publish(makeMarker(1,1,1,1,avgPos.x,avgPos.y))
+
+			#Calculate what movement should be classified as 
+			#the legs based on the distance a moving point is 
+			#from the average movement point
 			j=Point()
-			ft1=[]
-			ft2=[]
-			leg1Avg=Point()
-			leg2Avg=Point()
-			leg1xTot=0
-			leg1yTot=0
-			leg2xTot=0
-			leg2yTot=0
+			legs=[]
 			for j in positionsXY:
 				if(distanceTo(j,avgPos)<legsThreshold):
-					if(distanceTo(positionsXY[0],j)<perLegThreshold):
-						leg1xTot+=j.x
-						leg1yTot+=j.y
-						ft1.append(j)
-					else:
-						leg2xTot+=j.x
-						leg2yTot+=j.y
-						ft2.append(j)
-			# if(len(ft1)>0 and len(ft2)>0):
-			# 	leg1Avg.x=leg1xTot/len(ft1)
-			# 	leg1Avg.y=leg1yTot/len(ft1)
-			# 	leg2Avg.x=leg2xTot/len(ft2)
-			# 	leg2Avg.y=leg2yTot/len(ft2)
-			# 	pubMarker.publish(makeMarker(2,1,0,1,leg1Avg.x,leg1Avg.y))
-			# 	pubMarker.publish(makeMarker(3,0,1,1,leg2Avg.x,leg2Avg.y))
-			pubMarker.publish(makeSphereList(4,0,0,1,ft1))
-			pubMarker2.publish(makeSphereList(5,0,1,0,ft2))
+					legs.append(j)
+			pubMarker.publish(makeSphereList(4,0,0,1,legs))
+			
+#Initialize node and subscriber
 def mark():
-	rospy.init_node('mark', anonymous=True)
+	rospy.init_node('mark', anonymous=True) 
 	rospy.Subscriber('/scan', LaserScan, callback)
 	rospy.spin()
 if __name__ == '__main__':
